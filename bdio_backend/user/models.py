6fa2eff7c5145e1cc3dc1ebbf3a2b98f27607e63
PawelOchal <opawel262@gmail.com>
2024-03-24 00:37:19 +0100

    change user model, and validate email

--- bdio_backend/user/models.py
+++ bdio_backend/user/models.py
@@ -34,6 +34,15 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
+    def create_tutor(self, email, password):
+        """
+        Create and return a tutor
+        """
+        user = self.create_user(email, password)
+        user.is_tutor = True
+        user.save(using=self._db)
+        return user
+    
     def validate_email(self, email):
         """
         Validate email format
@@ -51,6 +60,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_tutor = models.BooleanField(default=False)
 
     objects = UserManager()
 
