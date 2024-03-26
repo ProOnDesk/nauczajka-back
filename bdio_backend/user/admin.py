59db25fd17b187497a83377f17faf2d22bd9dd0f
PawelOchal <opawel262@gmail.com>
2024-03-26 23:20:28 +0100

    add permission for tutor , signals for tutor model and some minor refactor of code

--- bdio_backend/user/admin.py
+++ bdio_backend/user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, TokenEmailConfirmation
+from .models import User, TokenEmailConfirmation, Tutor
 
 admin.site.register(User)
 admin.site.register(TokenEmailConfirmation)
+admin.site.register(Tutor)
