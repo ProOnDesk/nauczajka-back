afbedcd8ea70310fadec252440fe48cffcc59c58
PawelOchal <opawel262@gmail.com>
2024-04-14 21:23:51 +0200

    some small changes in models for chat

--- bdio_backend/chat/models.py
+++ bdio_backend/chat/models.py
@@ -11,13 +11,17 @@ class Conversation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        users_emails = " ".join(f'{user.email}, ' for user in self.users.all())
+        return f'{self.id} - {users_emails}'
+    
 class ConversationMessage(models.Model):
     """
     Conversation message model
     """
     id = models.UUIDField(primary_key=True, editable=False, unique=True, default=uuid.uuid4)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    username = models.CharField(max_length=255, blank=True)
