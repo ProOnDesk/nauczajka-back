2763a515605b8f8f49152ef7a75b20fe0b147fdb
PawelOchal <opawel262@gmail.com>
2024-04-14 21:23:06 +0200

    add endpoint for chat

---
+++ bdio_backend/chat/serializers.py
@@ -0,0 +1,74 @@
+from rest_framework import serializers
+from chat.models import Conversation, ConversationMessage
+from user.models import User
+from django.utils.translation import gettext as _
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the user object
+    """
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name',)
+        extra_kwargs = {
+            'id': {'read_only': False},
+            'first_name': {'read_only': True},
+            'last_name': {'read_only': True}
+        }
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the conversation object
+    """
+    users = UserSerializer(many=True, required=True)
+    
+    
+    class Meta:
+        model = Conversation
+        fields = '__all__'
+        extra_kwargs = {
+            'id': {'read_only': True},
+        }
+        
+    def create(self, validated_data):
+        users_data = validated_data.pop('users')
+        conversation = Conversation.objects.create(**validated_data)
+        for user_data in users_data:
+            user = User.objects.get(id=user_data['id'])
+            conversation.users.add(user)
+        return conversation
+
+    def validate_users(self, value):
+        if len(value) != 2:
+            raise serializers.ValidationError(_('Rozmowa musi mieć dokładnie dwóch użytkowników.'))
+        
+        if value[0]['id'] == value[1]['id']:
+            raise serializers.ValidationError(_('Rozmowa musi mieć dwóch różnych użytkowników.'))
+        
+        if not User.objects.filter(id=value[0]['id']).exists() or not User.objects.filter(id=value[1]['id']).exists():
+            raise serializers.ValidationError(_('Użytkownicy muszą istnieć.'))
+        
+        if Conversation.objects.filter(users__id=value[0]['id']).filter(users__id=value[1]['id']).exists():
+            raise serializers.ValidationError(_('Rozmowa już istnieje.'))
+        
+        return value
+            
+
+
+class ConversationMessagesSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the conversation message object
+    """
+    created_by = UserSerializer()
+    class Meta:
+        model = ConversationMessage
+        fields = ('id', 'conversation', 'body', 'created_at', 'created_by')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'conversation': {'read_only': True},
+            'created_at': {'read_only': True},
+            'username': {'read_only': True},
+        }
+        ordering = ['-created_at']
--- bdio_backend/chat/views.py
+++ bdio_backend/chat/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from chat.models import Conversation, ConversationMessage
+from chat.serializers import ConversationSerializer, ConversationMessagesSerializer
+from drf_spectacular.utils import extend_schema
+
+
+@extend_schema(tags=['Chat'])
+class ConversationCreateAPIView(APIView):
+    """
+    Create a conversation
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = ConversationSerializer
+    
+    def post(self, request):
+        user_id = request.user.id
+        data = request.data
+        data['users'].append({'id': user_id})
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@extend_schema(tags=['Chat'])
+class ConversationListAPIView(ListAPIView):
+    """
+    List all conversations
+    """
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.conversations.all()
+
+
+@extend_schema(tags=['Chat'])
+class ConversationDetailAPIView(APIView):
+    
+    serializer_class = ConversationMessagesSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, id):
+        conversation = Conversation.objects.get(id=id)
+        messages = ConversationMessage.objects.filter(conversation=conversation)
+        serializer = ConversationMessagesSerializer(messages, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
