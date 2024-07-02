cae0995681e16c13e8bd7956472c83d437188bfd
PawelOchal <opawel262@gmail.com>
2024-07-02 09:40:38 +0200

    adjustments for user model and code

--- bdio_backend/user/models.py
+++ bdio_backend/user/models.py
@@ -80,7 +80,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255, blank=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_tutor = models.BooleanField(default=False)
     is_confirmed = models.BooleanField(default=False)
