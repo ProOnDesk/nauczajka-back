65922f5dfa8f2fa837f88eedd95348ffbe93a75d
PawelOchal <opawel262@gmail.com>
2024-07-03 19:54:20 +0200

    add pagination for messages, chat list, tutor search

--- bdio_backend/user/models.py
+++ bdio_backend/user/models.py
@@ -79,7 +79,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255, blank=True)
-    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_tutor = models.BooleanField(default=False)
     is_confirmed = models.BooleanField(default=False)
