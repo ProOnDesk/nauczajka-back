86b7e705b01e77e4ecbf11aad2395c2f994d981b
PawelOchal <opawel262@gmail.com>
2024-07-08 20:47:14 +0200

    add utils for sending notification

---
+++ bdio_backend/notification/utils.py
@@ -0,0 +1,19 @@
+def send_general_notification(message):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        'notifications_general',
+        {
+            'type': 'send_notification',
+            'message': message,
+        }
+    )
+    
+def send_personal_notification(user, message):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f'notifications_{user.id}',
+        {
+            'type': 'send_notification',
+            'message': message,
+        }
+    )
