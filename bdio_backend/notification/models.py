cc355b12ed380a4c05216c36542cf62e59dd648c
PawelOchal <opawel262@gmail.com>
2024-07-10 20:05:52 +0200

    fix issue related to websocket notification and add sending notification in notification view for debugging

--- bdio_backend/notification/models.py
+++ bdio_backend/notification/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from user.models import User
 
 class Notification(models.Model):
-    user = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, related_name='notifications')
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- bdio_backend/notification/views.py
+++ bdio_backend/notification/views.py
@@ -3,11 +3,21 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from notification.serializers import NotificationSerializer
 from notification.models import Notification
+from notification.utils import send_personal_notification
+from notification.utils import send_personal_notification
+from rest_framework.response import Response
+
 
 class NotificationListAPIView(ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = NotificationSerializer
     
     def get_queryset(self):
-        return Notification.objects.filter(user=self.request.user).order_by('-created_at')
+        return Notification.objects.filter(users=self.request.user).order_by('-created_at')
+    
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
+        send_personal_notification(users=self.request.user, message="Wlasnie poprosiles o historie twoich powiadomien :)")
+        return Response(serializer.data)
     
