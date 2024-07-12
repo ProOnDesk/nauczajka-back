f6284cafe7fa60c8295685572b123a4f8c32ab20
PawelOchal <opawel262@gmail.com>
2024-07-12 13:02:10 +0200

    change pagination for tutor review

--- bdio_backend/core/pagination.py
+++ bdio_backend/core/pagination.py
@@ -1,7 +1,9 @@
 from rest_framework import pagination
+from tutor.models import Tutor
 
 class CustomPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 100
     page_query_param = 'p' 
+        
