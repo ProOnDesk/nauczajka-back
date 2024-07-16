a9bbd0a52048df3e5a04fc6aa5b301e76a9b7fc6
PawelOchal <opawel262@gmail.com>
2024-07-16 20:37:30 +0200

    fix max_age for cookie refresh token

--- bdio_backend/api/views.py
+++ bdio_backend/api/views.py
@@ -286,7 +286,7 @@ class CustomProviderAuthView(ProviderAuthView):
             response.set_cookie(
                 'refresh',
                 refresh_token,
-                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
