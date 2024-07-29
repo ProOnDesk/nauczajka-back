8bbea297e367cc38031020ad313f8b41a10b77f5
PawelOchal <opawel262@gmail.com>
2024-07-29 23:09:49 +0200

    update notification websockets

--- bdio_backend/notification/receivers.py
+++ bdio_backend/notification/receivers.py
@@ -1,18 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from notification.utils import send_unread_notification_count_to_channel
 from notification.models import UserNotification
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 @receiver(post_save, sender=UserNotification)
-def send_update_chat_to_channel(sender, instance, created, **kwargs):
-    user = instance.user
-    count = UserNotification.objects.filter(user=user, is_read=False).count()
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        f'notifications_{user.id}',
-        {
-            'type': 'get_unread_notification_count',
-            'unread_notification_count': count
-        }
-    )
+def send_unread_notification(sender, instance, created, **kwargs):
+    send_unread_notification_count_to_channel(instance.user)
+
