4ef6b9e8d714c9f8f47912832fcdbcee376ff386
PawelOchal <opawel262@gmail.com>
2024-10-17 20:05:47 +0200

    configure settings for production

--- bdio_backend/bdio_backend/asgi.py
+++ bdio_backend/bdio_backend/asgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,19 +18,19 @@ from chat import routing as chat_routing
 from notification import routing as notification_routing
 from core.middleware import TokenAuthMiddleware, CookieMiddleware
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bdio_backend.settings')
 
-application = get_asgi_application()
-
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http': application,
-    'websocket': CookieMiddleware(
-        TokenAuthMiddleware(
-            URLRouter(
-                chat_routing.websocket_urlpatterns + notification_routing.websocket_urlpatterns
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        CookieMiddleware(
+            TokenAuthMiddleware(
+                URLRouter(
+                    chat_routing.websocket_urlpatterns + notification_routing.websocket_urlpatterns
+                )
             )
         )
-    )
+    ),
 })
--- bdio_backend/bdio_backend/settings.py
+++ bdio_backend/bdio_backend/settings.py
@@ -27,17 +27,30 @@ load_dotenv()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG'))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG')) == 'True'
 
-if DEBUG:
-    FRONTED_URL = 'http://localhost:3000'
-    BACKEND_URL = 'http://localhost:8000'
+FRONTED_URL = os.environ.get('FRONTEND_URL')
+BACKEND_URL = os.environ.get('BACKEND_URL')
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
+# WITH PROXY
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    FRONTED_URL
+]
+CSRF_TRUSTED_ORIGINS = [
+    FRONTED_URL
+]
+
 
 INSTALLED_APPS = [
     # Third-party apps
